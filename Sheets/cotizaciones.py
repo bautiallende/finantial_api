f697736898ea9190694cc80bd21f9815bf379d83
bautiallende <bautiallende@hotmail.com>
2024-08-28 12:24:08 +0200

    Modificaciones para la actualizacion en conjunto

--- Sheets/cotizaciones.py
+++ Sheets/cotizaciones.py
@@ -16,10 +16,17 @@ def actualizar_cotizaciones():
     # Iterar sobre cada ticker y actualizar la información
     for i, (ticker, activo) in enumerate(zip(tickers, activos), start=8):
         valor_compra, valor_venta, fuente_dolar = obtener_cotizacion_dolar()
-        if valor_compra and valor_venta:
-            sheet.update_cell(i, 13, valor_compra)
-            sheet.update_cell(i, 14, valor_venta) 
-            sheet.update_cell(i, 19, fuente_dolar)
+
+        # Construir una lista para actualizar en bloque
+        fila_actualizar = [""] * 19
+        # Actualizar los valores comunes de la fila
+        fila_actualizar[0] = obtener_fecha_actual()  # Fecha
+        fila_actualizar[1] = ticker  # Ticker
+        if valor_compra and valor_venta:    
+            fila_actualizar[13] = valor_compra  # Tipo de Cambio Compra
+            fila_actualizar[14] = valor_venta  # Tipo de Cambio Venta
+            fila_actualizar[19] = fuente_dolar  # Fuente del Dólar
+
 
         if activo.lower() == "accion":
             print(f"Procesando {ticker} - {activo}")
@@ -39,6 +46,46 @@ def actualizar_cotizaciones():
             
 
             if resultados:
+                fila_actualizar[2] = resultados.get("nombre_completo", "")
+                fila_actualizar[3] = resultados.get("mercado", "")
+                fila_actualizar[4] = activo
+                fila_actualizar[5] = resultados.get("moneda", "")
+                fila_actualizar[6] = convertir_a_numero(resultados.get("precio_apertura", ""))
+                fila_actualizar[7] = convertir_a_numero(resultados.get("precio_cierre", ""))
+                fila_actualizar[8] = convertir_a_numero(resultados.get("precio_minimo", ""))
+                fila_actualizar[9] = convertir_a_numero(resultados.get("precio_maximo", ""))
+
+                cotizacion_moneda_local = convertir_a_numero(resultados.get("precio_actual", ""))
+                if cotizacion_moneda_local:
+                    if fila_actualizar[5] == "USD":
+                        fila_actualizar[12] = cotizacion_moneda_local
+                        if valor_compra:
+                            sheet.update_cell(i, 11, valor_compra * cotizacion_moneda_local)
+                    elif moneda == "ARS":
+                        sheet.update_cell(i, 11, cotizacion_moneda_local)
+                        if valor_venta:
+                            sheet.update_cell(i, 12, cotizacion_moneda_local/valor_venta)
+                    if precio_cierre:
+                        sheet.update_cell(i, 17, ((cotizacion_moneda_local - precio_cierre) / precio_cierre) )
+
+
+
+
+                fila_actualizar[10] = convertir_a_numero(resultados.get("precio_actual", ""))
+                
+                
+                
+                fila_actualizar[14] = convertir_a_numero(resultados.get("volumen", ""))
+                
+                fila_actualizar[15] = resultados.get("variacion_diaria", "")
+                
+                fila_actualizar[17] = resultados.get("fuente", "")
+
+
+
+
+
+
                 fecha = obtener_fecha_actual()
                 nombre_completo = resultados.get("nombre_completo","")
                 mercado = resultados.get("mercado","") 
